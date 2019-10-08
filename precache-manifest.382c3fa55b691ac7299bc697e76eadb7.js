28e031d88ecd00c5a74902509e68233443594972
Linh <nguyenlin19952@gmail.com>
2019-10-08 19:51:07 +0700

    Updates

---
+++ precache-manifest.382c3fa55b691ac7299bc697e76eadb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c22202ecd634d653fdb0e450de4e839",
+    "url": "/portolifo/index.html"
+  },
+  {
+    "revision": "7bc69a42ecc60479c5ad",
+    "url": "/portolifo/static/css/main.67847a88.chunk.css"
+  },
+  {
+    "revision": "b9a37567c7dc6f7ad741",
+    "url": "/portolifo/static/js/2.3f96fd53.chunk.js"
+  },
+  {
+    "revision": "7bc69a42ecc60479c5ad",
+    "url": "/portolifo/static/js/main.fdacb4a2.chunk.js"
+  },
+  {
+    "revision": "d9d171e951ecbc2c26b0",
+    "url": "/portolifo/static/js/runtime-main.3d51b657.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/portolifo/static/media/logo.25bf045c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.ff2165c960d58f1c439ab87e70d0e579.js"
+  "/portolifo/precache-manifest.382c3fa55b691ac7299bc697e76eadb7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-gh-pages/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portolifo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
